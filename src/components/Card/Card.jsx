e390ca28a795b982513f5ec89ac4c9145a307f7d
Kal2403 <kal2403199255@gmail.com>
2023-07-18 10:20:58 +0200

    Update components

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import style from './card.module.css';
+import { Link } from 'react-router-dom';
 
 const Card = ({ name, species, gender, onClose, image, origin, id}) => {
   return (
@@ -7,7 +8,9 @@ const Card = ({ name, species, gender, onClose, image, origin, id}) => {
          <div className={style.imageContainer}>
             <img className={style.cardImg} src={image} alt={name} />
             <div className={style.cardNameContainer}>
-               <h2 className={style.cardName}>{name}</h2>
+               <Link to={`/details/${id}`}>
+                  <h2 className={style.cardName}>{name}</h2>
+               </Link>
             </div>
             <button className={style.cardButton} onClick={() => onClose(id)}>X</button>
          </div>
--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 const Details = () => {
+
+  const {id} = useParams();
+
+  const [character, setCharacter] = useState({});
+
+  useEffect(() => {
+    axios(`https://rickandmortyapi.com/api/character/${id}`).then(({ data }) => {
+       if (data.name) {
+          setCharacter(data);
+       } else {
+          window.alert('No hay personajes con ese ID');
+       }
+    });
+    return setCharacter({});
+ }, [id]);
+
   return (
-    <div>Details</div>
+    <div>Soy el Details</div>
   )
 }
 
